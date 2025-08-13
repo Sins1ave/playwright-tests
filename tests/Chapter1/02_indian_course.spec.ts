464b22b27d6270fc434dc0942f87cf670f57418c
Sins1ave <mosindesigner@yandex.ru>
2025-08-13 21:05:36 +0300

    Написал кейс с поиском в гугл

--- tests/Chapter1/02_indian_course.spec.ts
+++ tests/Chapter1/02_indian_course.spec.ts
@@ -5,12 +5,16 @@ import { test, expect } from "@playwright/test";
 test("Валидация заголовка Web страницы", async ({ page }) => {
   // Go to URL
   await page.goto("https://www.google.com/?gws_rd=ssl");
+  await page.waitForTimeout(1000);
   // Search with keywords
   await page.getByRole("button", { name: "Отклонить все" }).click();
+  await page.waitForTimeout(1000);
   await page.getByRole("combobox", { name: "Найти" }).click();
+  await page.waitForTimeout(1000);
   await page
     .getByRole("combobox", { name: "Найти" })
     .fill("playwright by testers talk");
+  await page.waitForTimeout(1000);
   await page.getByRole("combobox", { name: "Найти" }).press("Enter");
 
   // Click on playlist

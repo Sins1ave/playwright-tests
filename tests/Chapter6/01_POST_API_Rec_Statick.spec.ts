8742207332a861501b50d910166e606ea5116b9c
Sins1ave <mosindesigner@yandex.ru>
2025-08-14 14:15:51 +0300

    Добавил доп.  проверки в первый API тест

--- tests/Chapter6/01_POST_API_Rec_Statick.spec.ts
+++ tests/Chapter6/01_POST_API_Rec_Statick.spec.ts
@@ -7,16 +7,36 @@ import postAPIRecuest from "../../API/test_data/api_requests/POST_API_Request.js
 test("Создание POST API  запроса изпользуя статический файл в плейрайт тайпскрипт", async ({
   request,
 }) => {
+  
+  //Create POST API Request
   const postAPIResponse = await request.post("/booking", {
     data: postAPIRecuest,
   });
+  
+  // Print JSON API Response
   const jsonPostAPIResponse = await postAPIResponse.json();
   console.log(
     "POST API Response : " + JSON.stringify(jsonPostAPIResponse, null, 2)
   );
+  
+  // Validating API Response
   expect(postAPIResponse.status()).toBe(200);
   expect(postAPIResponse.statusText()).toBe("OK");
   expect(postAPIResponse.headers()["content-type"]).toContain(
     "application/json"
   );
+
+  // Validate property/key names
+  expect(jsonPostAPIResponse.booking).toHaveProperty('firstname');
+  expect(jsonPostAPIResponse.booking).toHaveProperty('lastname');
+
+  expect(jsonPostAPIResponse.booking.bookingdates).toHaveProperty('checkin');
+  expect(jsonPostAPIResponse.booking.bookingdates).toHaveProperty('checkout');
+
+  // Validate API Response body 
+  expect(jsonPostAPIResponse.bookingid).toBeGreaterThan(0);
+  expect(jsonPostAPIResponse.booking.firstname).toBe('Vlad');
+  expect(jsonPostAPIResponse.booking.lastname).toBe('Playwrigt');
+
+
 });
